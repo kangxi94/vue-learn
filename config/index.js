ced85c5a8807ccb781e4ae47732311127c6c3994
kangxi <1075484233@qq.com>
2018-10-31 15:17:21 +0800

    Add github page

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/vue-learn/dist/',
     proxyTable: {},
 
     // Various Dev Server settings
